34f24ff555ba7d56ff9d92f62ea67bb2320c5a22
segostolyarov1993 <seregastolyarov46@gmail.com>
2025-10-13 15:01:16 +0300

    first commit

---
+++ lesson-01/01.js
@@ -0,0 +1,14 @@
+// С помощью цикла while найдите сумму чисел от 1 до 20
+// Результат присвойте переменной sum
+
+// your code
+let sum = 0;
+let min = 1;
+const max = 20;
+
+while (min <= max) {
+    sum = sum + min;
+    min = min + 1
+
+}
+
