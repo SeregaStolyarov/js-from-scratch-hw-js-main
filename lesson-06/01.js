c47b4dac4d9ee373a1465f654e517bd5496cdf80
segostolyarov1993 <seregastolyarov46@gmail.com>
2025-12-02 23:36:35 +0300

    les6

--- lesson-06/01.js
+++ lesson-06/01.js
@@ -22,5 +22,12 @@ const oddNumbers = filter(numbers, (element, index) => {
 
 console.log(oddNumbers) // Должен вывести: [1, 3, 5]
 */
+const numbers = [1, 2, 3, 4, 5]
+const filter = filter(numbers,(element,index) => {
+  return `${index}: ${element}`;
+  
+});
 
-const filter = () => {}
+console.log(filter);
+
+
