e58ca4c790c231c607a30ce498f59dc2a3f39d80
segostolyarov1993 <seregastolyarov46@gmail.com>
2025-12-19 18:38:41 +0300

    les7

--- lesson-07/01.js
+++ lesson-07/01.js
@@ -17,3 +17,31 @@ const WEB_TECH_IMAGES = [
   'https://production-it-incubator.s3.eu-central-1.amazonaws.com/file-manager/Image/c8a1f4a6-1337-4899-bdfd-a8c9c7bb806a_css-magic-logo.jpg',
   'https://production-it-incubator.s3.eu-central-1.amazonaws.com/file-manager/Image/784380b9-6937-42a6-bdfe-869835820234_html-magic-logo.jpg',
 ]
+
+const img = document.getElementById("web-tech-image")
+
+// 0,1,2
+
+const buttonPrev = document.getElementById("prev-button")
+const buttonNext = document.getElementById("next-button")
+
+let i = 0 
+
+buttonPrev.addEventListener('click',()=>{
+  i = i - 1 
+  if(i<0){
+    i = 2
+  }
+  img.src = WEB_TECH_IMAGES[i]
+  
+})
+
+buttonNext.addEventListener('click',()=>{
+  i = i + 1   
+  if(i > 2){
+    i = 0;
+  }
+img.src = WEB_TECH_IMAGES[i]
+})
+
+
